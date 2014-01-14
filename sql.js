97bce7992a9e57e2e0d1cf465b9bbea085840490
Cam Birch <cam@maintenanceconnection.ca>
2014-01-14 13:30:05 -0700

    Add javaDoc comments to the typescript file.

--- sql.js
+++ sql.js
@@ -4,16 +4,38 @@ var aps;
     var edge = require('edge');
 
     var conStr = '';
+
+    /**
+    * Must be called first.
+    * Sets the connection string to be used by all other calls to the aps-sql library.
+    *
+    * @param {string} str The connection string to use.
+    */
     function connectionString(str) {
         conStr = str;
     }
     aps.connectionString = connectionString;
 
+    /**
+    * Executes a query and returns the result.  Must have called 'connectionString()' first.
+    *
+    * *Important Note: converts all results from the database into JS.  Don't just return everything otherwise you may run out of memory.
+    *
+    * @param {string} command The SQL command string to run
+    * @param {object} params An optional object containing a list of @ parameters & values that are used in the previous command string.
+    * @returns An array of arrays.  First element contains the header row, all remaining arrays contain the data for the row.
+    */
     function executeQuery(command, params) {
         return functions({ fn: 'executeSync', conStr: conStr, command: command, params: params }, true).Result;
     }
     aps.executeQuery = executeQuery;
 
+    /**
+    * Executes a query that does not have a result.  Must have called 'connectionString()' first.
+    *
+    * @param {string} command The SQL command string to run
+    * @param {object} params An optional object containing a list of @ parameters & values that are used in the previous command string.
+    */
     function executeNonQuery(command, params) {
         functions({ fn: 'nonQuerySync', conStr: conStr, command: command, params: params }, true);
     }
--- sql.ts
+++ sql.ts
@@ -4,14 +4,35 @@ module aps {
     var edge = require('edge');
 
     var conStr: string = '';
+    /**
+     * Must be called first.
+     * Sets the connection string to be used by all other calls to the aps-sql library.
+     *
+     * @param {string} str The connection string to use.
+     */
     export function connectionString(str: string): void {
         conStr = str;
     }
 
+    /**
+     * Executes a query and returns the result.  Must have called 'connectionString()' first.
+     * 
+     * *Important Note: converts all results from the database into JS.  Don't just return everything otherwise you may run out of memory.
+     *
+     * @param {string} command The SQL command string to run
+     * @param {object} params An optional object containing a list of @ parameters & values that are used in the previous command string.
+     * @returns An array of arrays.  First element contains the header row, all remaining arrays contain the data for the row.
+     */
     export function executeQuery(command: string, params?: {}): any {
         return functions({ fn: 'executeSync', conStr: conStr, command: command, params: params}, true).Result;
     }
 
+    /**
+     * Executes a query that does not have a result.  Must have called 'connectionString()' first.
+     *
+     * @param {string} command The SQL command string to run
+     * @param {object} params An optional object containing a list of @ parameters & values that are used in the previous command string.
+     */
     export function executeNonQuery(command: string, params?: {}): void {
         functions({ fn: 'nonQuerySync', conStr: conStr, command: command, params: params }, true);
     }
