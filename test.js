cda77b93050943b3f5b8a1cb4a9dc444bd9ae4b1
Cam Birch <cam@maintenanceconnection.ca>
2014-01-31 09:53:39 -0700

    Update the test code to test creating a new transaction

--- test.js
+++ test.js
@@ -5,4 +5,8 @@ sql.connectionString('Data Source=devtest;Initial Catalog=entCustomer;User Id=sa
 console.log(sql.executeQuery('Select top 3 assetID from asset'));
 //sql.executeNonQuery();
 
+var transaction = sql.beginTransaction();
+console.log(transaction.executeQuery('Select top 3 assetID from asset'));
+transaction.cancel();
+
 console.log('done');
