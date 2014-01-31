393ff3a50d2caf9d67cdf46d6592916af4b0dc4e
Cam Birch <cam@maintenanceconnection.ca>
2014-01-31 09:47:07 -0700

    Include the new transaction support in the JS code.

--- sql.js
+++ sql.js
@@ -41,6 +41,61 @@ var aps;
     }
     aps.executeNonQuery = executeNonQuery;
 
+    /**
+    * Begins a new transaction.  Must have called 'connectionString()' first.
+    *
+    * @returns A Transaction object which can be used to execute queries, and commit or cancel the transaction.
+    */
+    function beginTransaction() {
+        var transCode = functions({ fn: 'beginTransaction', conStr: conStr }, true);
+        return new Transaction(transCode);
+    }
+    aps.beginTransaction = beginTransaction;
+
+    var Transaction = (function () {
+        function Transaction(transaction) {
+            this.transaction = transaction;
+        }
+        /**
+        * Executes a query and returns the result.
+        *
+        * *Important Note: converts all results from the database into JS.  Don't just return everything otherwise you may run out of memory.
+        *
+        * @param {string} command The SQL command string to run
+        * @param {object} params An optional object containing a list of @ parameters & values that are used in the previous command string.
+        * @returns An array of arrays.  First element contains the header row, all remaining arrays contain the data for the row.
+        */
+        Transaction.prototype.executeQuery = function (command, params) {
+            return functions({ fn: 'transactionExecuteSync', transaction: this.transaction, conStr: conStr, command: command, params: params }, true).Result;
+        };
+
+        /**
+        * Executes a query that does not have a result.
+        *
+        * @param {string} command The SQL command string to run
+        * @param {object} params An optional object containing a list of @ parameters & values that are used in the previous command string.
+        */
+        Transaction.prototype.executeNonQuery = function (command, params) {
+            return functions({ fn: 'transactionNonQuerySync', transaction: this.transaction, conStr: conStr, command: command, params: params }, true).Result;
+        };
+
+        /**
+        * Commits the transaction and closes the database connection.
+        */
+        Transaction.prototype.commit = function () {
+            return functions({ fn: 'commitTransaction', transaction: this.transaction }, true);
+        };
+
+        /**
+        * Rolls back the transaction and closes the database connection.
+        */
+        Transaction.prototype.cancel = function () {
+            return functions({ fn: 'cancelTransaction', transaction: this.transaction }, true);
+        };
+        return Transaction;
+    })();
+    aps.Transaction = Transaction;
+
     //#region Edge Function Inits
     var functions = edge.func({
         source: __dirname + "/.cs/Sql.cs",
@@ -53,3 +108,4 @@ var aps;
 exports.connectionString = aps.connectionString;
 exports.executeQuery = aps.executeQuery;
 exports.executeNonQuery = aps.executeNonQuery;
+exports.beginTransaction = aps.beginTransaction;
--- sql.ts
+++ sql.ts
@@ -37,6 +37,59 @@ module aps {
         functions({ fn: 'nonQuerySync', conStr: conStr, command: command, params: params }, true);
     }
 
+    /**
+     * Begins a new transaction.  Must have called 'connectionString()' first.
+     *
+     * @returns A Transaction object which can be used to execute queries, and commit or cancel the transaction.
+     */
+    export function beginTransaction(): Transaction {
+        var transCode = functions({ fn: 'beginTransaction', conStr: conStr }, true);
+        return new Transaction(transCode);
+    }
+
+
+    export class Transaction {
+        constructor(private transaction: any) {
+        }
+
+        /**
+         * Executes a query and returns the result.
+         * 
+         * *Important Note: converts all results from the database into JS.  Don't just return everything otherwise you may run out of memory.
+         *
+         * @param {string} command The SQL command string to run
+         * @param {object} params An optional object containing a list of @ parameters & values that are used in the previous command string.
+         * @returns An array of arrays.  First element contains the header row, all remaining arrays contain the data for the row.
+         */
+        public executeQuery(command: string, params?: {}): any {
+            return functions({ fn: 'transactionExecuteSync', transaction: this.transaction, conStr: conStr, command: command, params: params }, true).Result;
+        }
+        /**
+         * Executes a query that does not have a result.
+         *
+         * @param {string} command The SQL command string to run
+         * @param {object} params An optional object containing a list of @ parameters & values that are used in the previous command string.
+         */
+        public executeNonQuery(command: string, params?: {}): any {
+            return functions({ fn: 'transactionNonQuerySync', transaction: this.transaction, conStr: conStr, command: command, params: params }, true).Result;
+        }
+
+        /**
+         * Commits the transaction and closes the database connection.
+         */
+        public commit(): void {
+            return functions({ fn: 'commitTransaction', transaction: this.transaction }, true);
+        }
+        /**
+         * Rolls back the transaction and closes the database connection.
+         */
+        public cancel(): void {
+            return functions({ fn: 'cancelTransaction', transaction: this.transaction }, true);
+        }
+
+    }
+
+
     //#region Edge Function Inits
 
     var functions = edge.func({
@@ -53,3 +106,4 @@ module aps {
 exports.connectionString = aps.connectionString;
 exports.executeQuery = aps.executeQuery;
 exports.executeNonQuery = aps.executeNonQuery;
+exports.beginTransaction = aps.beginTransaction;
